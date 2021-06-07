2b26e607a6e4a9127fdcc0b9350b80be813f42aa
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-06-07 21:44:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,9 +2,9 @@
 from pyrogram import Client,idle
 import os
 
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
-SESSION_NAME = os.environ.get("SESSION_STRING")
+API_ID = int(os.environ.get("API_ID",12345))
+API_HASH = os.environ.get("API_HASH","")
+SESSION_NAME = os.environ.get("SESSION_NAME","")
 
 
 app = Client(SESSION_NAME, API_ID, API_HASH,plugins=dict(root="plugins"))
