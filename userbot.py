bee5c0b60618399e85bb6ff760a2651d6523c652
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-06-07 22:12:13 +0530

    Create userbot.py

---
+++ userbot.py
@@ -0,0 +1,9 @@
+from pyrogram import Client, idle
+
+app = Client("tgvc")
+# logging.basicConfig(level=logging.INFO)
+app.start()
+print('>>> USERBOT STARTED')
+idle()
+app.stop()
+print('\n>>> USERBOT STOPPED')
