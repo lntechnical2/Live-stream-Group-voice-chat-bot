a4b98c4e16ce95767befa12fec936d0b9bab1b73
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-06-07 21:44:34 +0530

    Delete userbot.py

--- userbot.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client, idle
-
-app = Client("tgvc")
-# logging.basicConfig(level=logging.INFO)
-app.start()
-print('>>> USERBOT STARTED')
-idle()
-app.stop()
-print('\n>>> USERBOT STOPPED')
