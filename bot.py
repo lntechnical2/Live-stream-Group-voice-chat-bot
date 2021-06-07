b08eb32e79a9059ae64625f306babc3b18b84b32
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-06-07 19:34:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,8 @@ API_ID = int(os.environ.get("API_ID", 12345))
 API_HASH = os.environ.get("API_HASH", "")
 SESSION_NAME = os.environ.get("SESSION_STRING", "")
 
-app = Client(SESSION_NAME, API_ID, API_HASH)
+app = Client( SESSION_NAME,API_ID,
+ API_HASH)
 
 HELP =""" Radio stations:
 
