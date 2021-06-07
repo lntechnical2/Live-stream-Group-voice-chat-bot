a74a51e8f37918f620b1d59969651a50d52e3a16
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-06-07 20:32:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ SESSION_NAME = os.environ.get("SESSION_STRING", "")
 
 
 app = Client(SESSION_NAME, API_ID, API_HASH,plugins=dict(root="plugins"))
+logging.basicConfig(level=logging.INFO)
 app.start()
 print('>>> USERBOT STARTED')
 idle()
