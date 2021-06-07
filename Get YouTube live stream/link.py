cf2af404e29d8ce90748b1df86a62e50d1cf4873
Lntechnical <82169706+lntechnical2@users.noreply.github.com>
2021-06-07 17:59:57 +0530

    Create link.py

---
+++ Get YouTube live stream/link.py
@@ -0,0 +1,9 @@
+import youtube_dl
+
+ydl_opts = {} 
+url = input("enter your url:- ")
+with youtube_dl.YoutubeDL(ydl_opts)as ydl:
+ meta = ydl.extract_info(url, download=False) 
+ formats = meta.get('formats', [meta]) 
+ for f in formats:
+  print(f['url'])
